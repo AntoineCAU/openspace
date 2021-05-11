c0e4efb09007d2f535eac42cdc14f776afd0744d
AntoineCau <acauwd@gmail.com>
2021-05-11 20:11:24 +0200

    adding best rated planets component

--- src/components/BestRatedPlanets.jsx
+++ src/components/BestRatedPlanets.jsx
@@ -3,9 +3,20 @@ import SBestRatedPlanets from './styled/SBestRatedPlanets';
 export default function Footer() {
   return (
     <SBestRatedPlanets>
-      <div className="top-rated-planets">Planet 1</div>
-      <div className="top-rated-planets">Planet 2</div>
-      <div className="top-rated-planets">Planet 3</div>
+      <section className="top-rated-planets">
+        <div className="planet-card">
+          <img className="planet-img" src="/img/earth.png" alt="earth" />
+          <article className="planet-name">Planet 1</article>
+        </div>
+        <div className="planet-card">
+          <img className="planet-img" src="/img/uranus.jpg" alt="uranus" />
+          <article className="planet-name">Planet 2</article>
+        </div>
+        <div className="planet-card">
+          <img className="planet-img" src="/img/sun.jpg" alt="sun" />
+          <article className="planet-name">Planet 3</article>
+        </div>
+      </section>
     </SBestRatedPlanets>
   );
 }
---
+++ src/components/styled/SBestRatedPlanets.jsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+const SBestRatedPlanets = styled.div`
+  .top-rated-planets {
+    display: flex;
+    justify-content: center;
+    margin: 30px 200px;
+    .planet-card {
+      width: 300px;
+      height: 400px;
+      margin: 20px;
+      border: black solid 2px;
+      border-radius: 15px;
+      .planet-img {
+        width: 100%;
+        border-radius: 12px;
+      }
+    }
+  }
+`;
+
+export default SBestRatedPlanets;

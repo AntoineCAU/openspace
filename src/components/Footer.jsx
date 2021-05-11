a42090e33b47c7e0ec55bbdf56314320f675232e
AntoineCau <acauwd@gmail.com>
2021-05-11 18:16:56 +0200

    adding footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,43 @@
+import { NavLink } from 'react-router-dom';
 import SFooter from './styled/SFooter';
 
 export default function Footer() {
   return (
     <SFooter>
-      <p>Copyright © Open Space wahou incroyable (je c pa koi ékrir)</p>
+      <nav>
+        <ul className="foot-list">
+          <li className="foot-item about">
+            <NavLink
+              to="/about-us"
+              className="footlink about"
+              activeClassName="selected"
+            >
+              About Us
+            </NavLink>
+          </li>
+          <li className="foot-item ressources">
+            <NavLink
+              to="/ressources"
+              className="footlink ressources"
+              activeClassName="selected"
+            >
+              Ressources
+            </NavLink>
+          </li>
+          <li className="foot-item contact">
+            <NavLink
+              to="/contact-us"
+              className="footlink contact"
+              activeClassName="selected"
+            >
+              Contact Us
+            </NavLink>
+          </li>
+        </ul>
+        <div className="foot-copyright">
+          Hackathon 2021 - Wild Code School Lille - Open Space
+        </div>
+      </nav>
     </SFooter>
   );
 }
--- src/components/styled/SFooter.jsx
+++ src/components/styled/SFooter.jsx
@@ -1,5 +1,39 @@
 import styled from 'styled-components';
 
-const SFooter = styled.footer``;
+const SFooter = styled.footer`
+  nav {
+    background-color: #061028;
+    height: 120px;
+    .foot-copyright {
+      display: flex;
+      justify-content: center;
+      color: #fff;
+      font-size: 15px;
+      padding-top: 15px;
+    }
+    .foot-list {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      list-style-type: none;
+      .foot-item {
+        padding: 10px 0;
+        margin: 15px;
+        &.ressources {
+          border-left: 1px solid #fff;
+          border-right: 1px solid #fff;
+          padding: 10px 15px;
+          margin: 0 15px;
+        }
+        .footlink {
+          text-decoration: none;
+          font-weight: 700;
+          font-size: 20px;
+          color: #fff;
+        }
+      }
+    }
+  }
+`;
 
 export default SFooter;

e7c68a29d218787e52257261456b6f60d492dac9
AntoineCau <acauwd@gmail.com>
2021-05-11 19:03:30 +0200

    adding best rated planets component

---
+++ src/components/BestRatedPlanets.jsx
@@ -0,0 +1,11 @@
+import SBestRatedPlanets from './styled/SBestRatedPlanets';
+
+export default function Footer() {
+  return (
+    <SBestRatedPlanets>
+      <div className="top-rated-planets">Planet 1</div>
+      <div className="top-rated-planets">Planet 2</div>
+      <div className="top-rated-planets">Planet 3</div>
+    </SBestRatedPlanets>
+  );
+}

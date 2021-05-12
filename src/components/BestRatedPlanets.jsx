17aae94627ca7731c60a5bde0ed87fa4b9f344ee
AntoineCau <acauwd@gmail.com>
2021-05-12 10:48:58 +0200

    add links on BestRatedPlanets & Destinations

--- src/components/BestRatedPlanets.jsx
+++ src/components/BestRatedPlanets.jsx
@@ -7,24 +7,21 @@ export default function Footer() {
       <h1 className="top-rated-title"> Top Rated Planets</h1>
       <div className="top-rated-planets">
         <div className="planet-card">
-          <img className="planet-img" src="/img/earth.png" alt="earth" />
-          <article className="planet-name">Earth</article>
-          <Link to="/planet/earth" className="discover">
-            Discover
+          <Link to="/planet/earth" className="top-planets-link">
+            <img className="planet-img" src="/img/earth.png" alt="earth" />
+            <article className="planet-name">Earth</article>
           </Link>
         </div>
         <div className="planet-card">
-          <img className="planet-img" src="/img/uranus.jpg" alt="uranus" />
-          <article className="planet-name">Uranus</article>
-          <Link to="/planet/uranus" className="discover">
-            Discover
+          <Link to="/planet/uranus" className="top-planets-link">
+            <img className="planet-img" src="/img/uranus.jpg" alt="uranus" />
+            <article className="planet-name">Uranus</article>
           </Link>
         </div>
         <div className="planet-card">
-          <img className="planet-img" src="/img/sun.jpg" alt="sun" />
-          <article className="planet-name">The Sun</article>
-          <Link to="/planet/sun" className="discover">
-            Discover
+          <Link to="/planet/sun" className="top-planets-link">
+            <img className="planet-img" src="/img/sun.jpg" alt="sun" />
+            <article className="planet-name">The Sun</article>
           </Link>
         </div>
       </div>
--- src/components/DestinationsPage.jsx
+++ src/components/DestinationsPage.jsx
@@ -19,85 +19,76 @@ export default function DestinationsPage() {
 
         <section className="planets">
           <div className="sun planet">
-            <img src="/img/sun.jpg" alt="" />
-            <div className="text">
-              <h3>The Sun</h3>
-              <Link to="/planet/sun" className="discover">
-                Discover
-              </Link>
-            </div>
+            <Link to="/planet/sun" className="discover">
+              <img src="/img/sun.jpg" alt="" />
+              <div className="text">
+                <h3>The Sun</h3>
+              </div>
+            </Link>
           </div>
           <div className="mercury planet">
-            <img src="/img/mercury.jpg" alt="" />
-            <div className="text">
-              <h3>Mercury</h3>
-              <Link to="/planet/mercury" className="discover">
-                Discover
-              </Link>
-            </div>
+            <Link to="/planet/mercury" className="discover">
+              <img src="/img/mercury.jpg" alt="" />
+              <div className="text">
+                <h3>Mercury</h3>
+              </div>
+            </Link>
           </div>
           <div className="venus planet">
-            <img src="/img/venus.jpg" alt="" />
-            <div className="text">
-              <h3>Venus</h3>
-              <Link to="/planet/venus" className="discover">
-                Discover
-              </Link>
-            </div>
+            <Link to="/planet/venus" className="discover">
+              <img src="/img/venus.jpg" alt="" />
+              <div className="text">
+                <h3>Venus</h3>
+              </div>
+            </Link>
           </div>
           <div className="earth planet">
-            <img src="/img/earth.png" alt="" />
-            <div className="text">
-              <h3>Earth</h3>
-              <Link to="/planet/earth" className="discover">
-                Discover
-              </Link>
-            </div>
+            <Link to="/planet/earth" className="discover">
+              <img src="/img/earth.png" alt="" />
+              <div className="text">
+                <h3>Earth</h3>
+              </div>
+            </Link>
           </div>
           <div className="mars planet">
-            <img src="/img/mars.jpg" alt="" />
-            <div className="text">
-              <h3>Mars</h3>
-              <Link to="/planet/mars" className="discover">
-                Discover
-              </Link>
-            </div>
+            <Link to="/planet/mars" className="discover">
+              <img src="/img/mars.jpg" alt="" />
+              <div className="text">
+                <h3>Mars</h3>
+              </div>
+            </Link>
           </div>
           <div className="jupiter planet">
-            <img src="/img/jupiter.jpg" alt="" />
-            <div className="text">
-              <h3>Jupiter</h3>
-              <Link to="/planet/jupiter" className="discover">
-                Discover
-              </Link>
-            </div>
+            <Link to="/planet/jupiter" className="discover">
+              <img src="/img/jupiter.jpg" alt="" />
+              <div className="text">
+                <h3>Jupiter</h3>
+              </div>
+            </Link>
           </div>
           <div className="saturn planet">
-            <img src="/img/saturn.jpg" alt="" className="saturnImg" />
-            <div className="text">
-              <h3>Saturn</h3>
-              <Link to="/planet/saturn" className="discover">
-                Discover
-              </Link>
-            </div>
+            <Link to="/planet/saturn" className="discover">
+              <img src="/img/saturn.jpg" alt="" className="saturnImg" />
+              <div className="text">
+                <h3>Saturn</h3>
+              </div>
+            </Link>
           </div>
           <div className="uranus planet">
-            <img src="/img/uranus.jpg" alt="" />
-            <div className="text">
-              <h3>Uranus</h3>
-              <Link to="/planet/uranus" className="discover">
-                Discover
-              </Link>
-            </div>
+            <Link to="/planet/uranus" className="discover">
+              <img src="/img/uranus.jpg" alt="" />
+              <div className="text">
+                <h3>Uranus</h3>
+              </div>
+            </Link>
           </div>
           <div className="neptune planet">
-            <img src="/img/neptune.jpg" alt="" />
-            <div className="text">
-              <h3>Neptune</h3>
-              <Link to="/planet/neptune" className="discover">
-                Discover
-              </Link>
-            </div>
+            <Link to="/planet/neptune" className="discover">
+              <img src="/img/neptune.jpg" alt="" />
+              <div className="text">
+                <h3>Neptune</h3>
+              </div>
+            </Link>
           </div>
         </section>
       </SDestinations>

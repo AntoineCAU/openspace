ecfe414cad8aaa560ade0c97037578b1c8366747
AntoineCau <acauwd@gmail.com>
2021-05-12 02:54:12 +0200

    getting only 4 latest news

--- src/components/SpaceNews.jsx
+++ src/components/SpaceNews.jsx
@@ -5,14 +5,25 @@ import SSpaceNews from './styled/SSpaceNews';
 
 export default function SpaceNews() {
   const [news, setNews] = useState([]);
+  const id = [
+    '609b075ac2ea06001d94fa56',
+    '609af244c2ea06001d94fa54',
+    '609ae6a4c2ea06001d94fa53',
+    '609ac5bbc2ea06001d94fa52',
+  ];
 
   useEffect(() => {
-    axios
-      .get(`https://www.spaceflightnewsapi.net/api/v2/articles`)
-      .then(({ data }) => {
-        console.log(data);
-        setNews(data);
-      });
+    const articles = [];
+    for (let i = 0; i < id.length; i += 1) {
+      axios
+        .get(`https://www.spaceflightnewsapi.net/api/v2/articles/${id[i]}`)
+        .then(({ data }) => {
+          articles.push(data);
+        });
+      setTimeout(() => {
+        setNews(articles);
+      }, 1000);
+    }
   }, []);
 
   return (
